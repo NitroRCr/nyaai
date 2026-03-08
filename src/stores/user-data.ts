6d432474b9f95a3a457de1838c76294c72acb3fb
NitroRCr <i@krytro.com>
2026-03-08 10:57:43 +0800

    update

--- src/stores/user-data.ts
+++ src/stores/user-data.ts
@@ -1,13 +1,12 @@
 import { defineStore, acceptHMRUpdate } from 'pinia'
-import { mutate } from '../utils/zero-session'
+import { mutate, user } from '../utils/zero-session'
 import { useQuery } from 'src/composables/zero/query'
 import { queries } from 'app/src-shared/queries'
 import { mutators } from 'app/src-shared/mutators'
 import { computed } from 'vue'
-import { session } from 'src/utils/auth-client'
 
 export const useUserDataStore = defineStore('user-data', () => {
-  const { data, status } = useQuery(() => session.value.data ? queries.userData() : null)
+  const { data, status } = useQuery(() => user.id ? queries.userData() : null)
 
   async function updateData(updates: Record<string, any>) {
     await mutate(mutators.updateUserData({ updates })).client
