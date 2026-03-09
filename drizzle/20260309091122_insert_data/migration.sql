1d8386fea584c71b071c623d9a3d8b9cc992e62f
NitroRCr <i@krytro.com>
2026-03-09 17:50:10 +0800

    fix migration

--- drizzle/20260309091122_insert_data/migration.sql
+++ drizzle/20260309091122_insert_data/migration.sql
@@ -1,6 +1,6 @@
 -- Custom SQL migration file --
 INSERT INTO "mergePatchesRule" ("offset", "interval", "gap", "lastTime") VALUES
-  (2*60*60*1000,       10*60*1000,        1*60*1000,        EXTRACT(EPOCH FROM NOW())::bigint * 1000),
-  (2*24*60*60*1000,    1*24*60*60*1000,   1*60*60*1000,     EXTRACT(EPOCH FROM NOW())::bigint * 1000),
-  (30*24*60*60*1000,   7*24*60*60*1000,   1*24*60*60*1000,  EXTRACT(EPOCH FROM NOW())::bigint * 1000)
+  (7200000,        600000,        60000,        EXTRACT(EPOCH FROM NOW())::bigint * 1000),
+  (172800000,      86400000,      3600000,      EXTRACT(EPOCH FROM NOW())::bigint * 1000),
+  (2592000000,     604800000,     86400000,     EXTRACT(EPOCH FROM NOW())::bigint * 1000)
 ON CONFLICT DO NOTHING;
